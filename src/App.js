6d93d3df927f0894b45fe454fe5202459461d90e
Sumit Kumar <sumitkumar14695@gmail.com>
2024-03-17 00:30:15 +0530

    refactor

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ function App() {
   return (
     <div className="App">
       <AddTask/>
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
